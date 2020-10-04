865d166ab4274e734efc9dd202547385482bd8ec
lyckelvin <uni_xp@163.com>
2020-10-05 01:09:23 +0800

    second commit

--- app/src/main/java/com/example/sunnyweather/MainActivity.kt
+++ app/src/main/java/com/example/sunnyweather/MainActivity.kt
@@ -2,10 +2,13 @@ package com.example.sunnyweather
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.lifecycle.ViewModelProvider
 
 class MainActivity : AppCompatActivity() {
+    //private lateinit var myViewModel:MyViewModel
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        //myViewModel = ViewModelProvider(this)[MyViewModel::class.java]
     }
 }
---
+++ app/src/main/java/com/example/sunnyweather/logic/network/PlaceService.kt
@@ -0,0 +1,13 @@
+package com.example.sunnyweather.logic.network
+
+
+import retrofit2.Call
+import com.example.sunnyweather.SunnyWeatherApplication
+import com.example.sunnyweather.logic.model.PlaceResponse
+import retrofit2.http.GET
+import retrofit2.http.Query
+
+interface PlaceService {
+    @GET("v2/place?token=${SunnyWeatherApplication.TOKEN}&lang=zh_CN")
+    fun searchPlaces(@Query("query") query:String): Call<PlaceResponse>
+}
